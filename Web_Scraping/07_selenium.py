f17962ab363bd4f64210ab4aa5334586a2de2486
anonymo2239 <alperen.arda.adem22@gmail.com>
2025-02-09 15:24:54 +0300

    xpath

---
+++ Web_Scraping/07_selenium.py
@@ -0,0 +1,3 @@
+# Bir web sitesini taramak için selenyum gerekip gerekmediğini öğrenmek istediğinizde,
+# JavaScript'i devre dışı bırakın(sağ üst ayarlar > disable javascript) ve sonra web
+# sitesinin istediğiniz verileri yüklemek için JavaScript'e ihtiyacı olup olmadığına bakın.
