e124f4c319b9598259a5cbd80bafdb539e2eb3e9
anonymo2239 <alperen.arda.adem22@gmail.com>
2025-02-18 23:46:50 +0300

    pagination

--- Web_Scraping/11_P2_pagination.py
+++ Web_Scraping/11_P2_pagination.py
@@ -1,15 +1,50 @@
+import chromedriver_autoinstaller
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import chromedriver_autoinstaller
 
+# Headless mode, tarayıcıyı görsel arayüz olmadan (arka planda) çalıştıran bir moddur.
+# Daha hızlı çalışır çünkü görsel öğeleri yüklemez.
+# Sistem kaynaklarını daha az tüketir.
+# Sunucuda çalıştırılabilir
+# options = Options()
+# options.add_argument("--headless")  # Burada aktif ediyoruz.
+# options.add_argument('window-size=1920x1080')  # zounlu degil eklenebilir
+# simdilik kullanmaya gerek yok
 chromedriver_autoinstaller.install()
 
 web = "https://www.audible.com/search"
+# driver = webdriver.Chrome(options=options)  # headless mode için
 driver = webdriver.Chrome()
 driver.get(web)
+# driver.maximize_window()  # headless mode calisirken guzel calismaz
 
-driver.maximize_window()
+pagination = driver.find_element(By.XPATH, '//ul[contains(@class, "pagingElements")]')
+pages = pagination.find_elements(By.TAG_NAME, 'li')
+last_page = int(pages[-2].text)  # son sayfanin numarasini aldik
+current_page = 1
 
-container = driver.find_element(By.CLASS_NAME, "adbl-impression-container ")
-container.find_element(By.XPATH, "")
+book_title = []
+book_author = []
+book_length = []
 
+while current_page <= last_page:
+    container = driver.find_element(By.XPATH, '/html/body/div[1]/div[5]/div[5]/div/'
+                                              'div[2]/div[4]/div/div/div/span[2]/ul')
+    products = container.find_elements(By.XPATH, "./li")  # find_elements bir liste
+    # döndürür ve loop ile listeyi gezeriz.
+
+    for product in products:
+        book_title.append(product.find_element(By.XPATH, './/h3[contains(@class, "bc-heading")]').text)
+        book_author.append(product.find_element(By.XPATH, './/li[contains(@class, "authorLabel")]').text)
+        book_length.append(product.find_element(By.XPATH, './/li[contains(@class, "runtimeLabel")]').text)
+
+    current_page += 1
+    try:
+        next_page = driver.find_element(By.XPATH, '//span[contains(@class, "nextButton")]')
+        next_page.click()
+    except:
+        pass
+
+book_sheet = pd.DataFrame({'title': book_title, 'author': book_author, 'length': book_length})
+book_sheet.to_csv('bookList.csv', index=False)
